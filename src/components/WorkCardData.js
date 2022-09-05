5287f3d66dfb595282fb7673e6bc962c33595648
MohammedSherwan <muhammed.sh543@gmail.com>
2022-09-05 23:38:24 +0300

    deploy my app to github pages

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,37 @@
+import pro1 from "../assets/image5.jpg";
+import pro2 from "../assets/image5.jpg";
+import pro3 from "../assets/image5.jpg";
+import pro4 from "../assets/image5.jpg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecommerce Website Design",
+    text: "This is a online ordering website wich you can order any receiver device you want and send your address to deliver to you",
+    view: "#",
+    source: "#",
+  },
+  {
+    imgsrc: pro2,
+    title: "Service Website Design",
+    text: "This is a online ordering website wich you can order any receiver device you want and send your address to deliver to you",
+    view: "#",
+    source: "#",
+  },
+  {
+    imgsrc: pro3,
+    title: "Hotel Website Design",
+    text: "This is a online ordering website wich you can order any receiver device you want and send your address to deliver to you",
+    view: "#",
+    source: "#",
+  },
+  {
+    imgsrc: pro4,
+    title: "Hotel Website Design",
+    text: "This is a online ordering website wich you can order any receiver device you want and send your address to deliver to you",
+    view: "#",
+    source: "#",
+  },
+];
+
+export default ProjectCardData;
